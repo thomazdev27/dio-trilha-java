95291f7c3ccd15b553bed2b7c02291cbc312a8b2
thomazdev27 <thomazd721@gmail.com>
2023-03-20 23:15:27 -0300

    exemplos

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,12 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+     
+     // TiposEVariaveis.java
+
+short numeroCurto = 1;
+int numeroNormal = numeroCurto;
+short numeroCurto2 = numeroNormal;
+
+        System.out.println("Hello, World!");
+    }
+}
